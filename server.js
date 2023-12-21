ff9b30359efefcf3c25dd2266091514ed3404c56
AjiteshSaranathK <152952302+AjiteshSaranathK@users.noreply.github.com>
2023-12-21 13:09:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js microservice! for testing?TEST22');
+});
+
+app.get('/hi',function(req,res){
+    res.send('{"response":"Hi Everyone, Hope everything is fine and all is well,well,well"}');
+})
+
+app.get('/ready',function(req,res){
+    res.send('{"responses":"Ready for the test ride!"}');
+})
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+module.exports=app;
